94af2779b38979ea5b811a748459b0a5071f2d0c
redtachyon <ariel.j.kwiatkowski@gmail.com>
2021-09-11 22:03:08 +0300

    More restructuring

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='coltra-rl',
     version='0.1.0',
-    packages=['coltra', 'coltra.envs', 'coltra.tests', 'coltra.models'],
+    packages=['coltra', 'coltra.envs', 'coltra.models'],
     url='https://github.com/redtachyon/coltra-rl',
     license='GNU GPLv3',
     author='RedTachyon',
