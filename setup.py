6034880edd681edaed1eb894ca2987bcff4156a2
redtachyon <ariel.j.kwiatkowski@gmail.com>
2021-09-11 21:56:58 +0300

    More restructuring

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='coltra-rl',
+    version='0.1.0',
+    packages=['coltra', 'coltra.envs', 'coltra.tests', 'coltra.models'],
+    url='https://github.com/redtachyon/coltra-rl',
+    license='GNU GPLv3',
+    author='RedTachyon',
+    author_email='ariel.j.kwiatkowski@gmail.com',
+    description='Coltra-RL is a simple moddable RL algorithm implementation'
+)
