3b634161e27ae1c833273bebd7904102da991a68
redtachyon <ariel.j.kwiatkowski@gmail.com>
2021-10-07 13:49:13 +0200

    Comment out the WIP PZ because it fails pytype

--- coltra/envs/pettingzoo_envs.py
+++ coltra/envs/pettingzoo_envs.py
@@ -8,26 +8,26 @@ from coltra.envs import MultiAgentEnv
 from coltra.envs.base_env import VecEnv, ActionDict, StepReturn, ObsDict
 
 
-class PettingZooEnv(MultiAgentEnv):
-    """WIP"""
-
-    def __init__(self, env_creator: Callable[[Any], ParallelEnv], **kwargs):
-        super().__init__(**kwargs)
-        self.s_env = env_creator(**kwargs)
-
-    def _embed_observation(self, obs: np.ndarray) -> Observation:
-        pass
-
-    def reset(self, *args, **kwargs) -> ObsDict:
-        obs = self.s_env.reset()
-        return {key: Observation(vector=obs[key]) for key in obs}
-
-    def step(self, action_dict: ActionDict) -> StepReturn:
-        pass
-
-    def render(self, mode="rgb_array"):
-        pass
-
-    @classmethod
-    def get_venv(cls, workers: int = 8, *args, **kwargs) -> VecEnv:
-        pass
+# class PettingZooEnv(MultiAgentEnv):
+#     """WIP"""
+#
+#     def __init__(self, env_creator: Callable[[Any], ParallelEnv], **kwargs):
+#         super().__init__(**kwargs)
+#         self.s_env = env_creator(**kwargs)
+#
+#     def _embed_observation(self, obs: np.ndarray) -> Observation:
+#         pass
+#
+#     def reset(self, *args, **kwargs) -> ObsDict:
+#         obs = self.s_env.reset()
+#         return {key: Observation(vector=obs[key]) for key in obs}
+#
+#     def step(self, action_dict: ActionDict) -> StepReturn:
+#         pass
+#
+#     def render(self, mode="rgb_array"):
+#         pass
+#
+#     @classmethod
+#     def get_venv(cls, workers: int = 8, *args, **kwargs) -> VecEnv:
+#         pass
