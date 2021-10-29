4adc63735260d0ec286d5db21b3eb9a253dd6e66
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2021-10-29 21:32:09 +0200

    Minor fix

--- coltra/configs.py
+++ coltra/configs.py
@@ -79,6 +79,8 @@ class RelationConfig(BaseConfig):
     num_actions: int = 2
     rel_input_size: int = 4
 
+    sigma0: float = 0.0
+
     vec_hidden_layers: List[int] = [32, 32]
     rel_hidden_layers: List[int] = [32, 32]
     com_hidden_layers: List[int] = [32, 32]
