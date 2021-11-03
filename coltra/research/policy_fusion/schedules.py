ecba1ce97bd01631cb7736ddaecb53b3fd7b656e
akwiatkowski <ariel.kwiatkowski@ubisoft.com>
2021-11-03 17:35:05 +0100

    progress on schedules

--- coltra/research/policy_fusion/schedules.py
+++ coltra/research/policy_fusion/schedules.py
@@ -1,8 +1,29 @@
+from types import SimpleNamespace
+
 SCHEDULE_0 = [  # Empty schedule
     # Format:
-    #  (time, do_clone, [freeze], coefficient)
+    #  (time, do_clone, [freeze], name, coefficient)
 ]
 
-SCHEDULE_1 = [
+Schedules = SimpleNamespace(
+    CrowdSchedules=[
+        [
+            (1, False, [False], "collision", 0.0),
+            (100, True, [True, False], "collision", -0.6),
+            (500, False, [False, False], "collision", -0.3)
+        ],
 
-]
+        [
+            (1, False, [False], "collision", 0.0),
+            (100, True, [True, False], "collision", -0.3),
+            (500, False, [False, False], "collision", -0.3)
+        ],
+    ],
+    SmartNavSchedules=[
+        [
+            (1, False, [False], "visible_reward", 0.0),
+            (300, True, [True, False], "visible_reward", -0.005),
+            (600, False, [False, False], "visible_reward", -0.001)
+        ]
+    ]
+)
