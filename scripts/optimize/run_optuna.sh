8011292bb6cd09f7e179c222d5c6b5fe75caccd8
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-03-18 00:29:01 +0100

    4 in parallel

--- scripts/optimize/run_optuna.sh
+++ scripts/optimize/run_optuna.sh
@@ -9,17 +9,4 @@ sleep 1
 python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 20 -n 1 -o test &
 sleep 1
 python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 30 -n 1 -o test &
-sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 40 -n 1 -o test &
-sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 50 -n 1 -o test &
-sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 60 -n 1 -o test &
-sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 70 -n 1 -o test &
-sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 80 -n 1 -o test &
-sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 90 -n 1 -o test &
-sleep 1
 wait
