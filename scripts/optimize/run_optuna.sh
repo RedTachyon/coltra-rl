284d1180012d00a521e1dbf38e676833c5174d5b
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-03-18 00:21:30 +0100

    10 in parallel

--- scripts/optimize/run_optuna.sh
+++ scripts/optimize/run_optuna.sh
@@ -2,7 +2,24 @@ COLTRA=$WORK/coltra-rl
 PROJECT=$COLTRA/scripts
 
 
-srun python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 0 -n 1 -o test &
-sleep 5
-srun python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 10 -n 1 -o test &
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 0 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 10 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 20 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 30 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 40 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 50 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 60 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 70 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 80 -n 1 -o test &
+sleep 1
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 90 -n 1 -o test &
+sleep 1
 wait
