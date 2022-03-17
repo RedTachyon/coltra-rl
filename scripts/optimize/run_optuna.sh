f050b8424695ba8dd12cb524530bb9be20292c0d
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-03-17 23:32:39 +0100

    split the sbatch

--- scripts/optimize/run_optuna.sh
+++ scripts/optimize/run_optuna.sh
@@ -1,1 +1,8 @@
-CUDA_VISIBLE_DEVICES=$1 python optuna_crowd.py -e /home/ariel/projects/coltra-rl/builds/crowd-v5/crowd.x86_64 -w $2 -n 25 -o absolute
+COLTRA=$WORK/coltra-rl
+PROJECT=$COLTRA/scripts
+
+
+srun python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 0 -n 1 -o test &
+sleep 5
+srun python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 10 -n 1 -o test &
+wait
