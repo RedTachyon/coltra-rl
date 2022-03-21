5321b379549eb4ecc5dd3d52b6d54438649a2b08
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-03-21 18:30:52 +0100

    Another try

--- scripts/optimize/run_sbatch.sh
+++ scripts/optimize/run_sbatch.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/bash
 
 LASTID1=$(sbatch crowd.sbatch)
+LASTID1=$(echo $LASTID1 | awk 'NF{ print $NF }')
+
 echo "Launched $LASTID1"
 LASTID2=$(sbatch crowd.sbatch)
+LASTID2=$(echo $LASTID2 | awk 'NF{ print $NF }')
+
 echo "Launched $LASTID2"
 
 for i in {2..15}
 do
     LASTID1=$(sbatch --dependency=afterok:$LASTID1 crowd.sbatch)
+    LASTID1=$(echo $LASTID1 | awk 'NF{ print $NF }')
     echo "$i Queued $LASTID1"
 
     LASTID2=$(sbatch --dependency=afterok:$LASTID2 crowd.sbatch)
+    LASTID2=$(echo $LASTID2 | awk 'NF{ print $NF }')
+
     echo "$i Queued $LASTID2"
 done
