75dbcbf8f8248aac9e64d2e316218013a77584d0
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-03-21 18:24:44 +0100

    Full sbatch sweep script?

--- scripts/optimize/run_sbatch.sh
+++ scripts/optimize/run_sbatch.sh
@@ -1,2 +1,15 @@
-sbatch crowd.sbatch
-sbatch --dependency=afterok:540417 crowd.sbatch
+#!/usr/bin/bash
+
+LASTID1=$(sbatch crowd.sbatch)
+echo "Launched $LASTID1"
+LASTID2=$(sbatch crowd.sbatch)
+echo "Launched $LASTID2"
+
+for i in {2..15}
+do
+    LASTID1=$(sbatch --dependency=afterok:$LASTID1 crowd.sbatch)
+    echo "$i Queued $LASTID1"
+
+    LASTID2=$(sbatch --dependency=afterok:$LASTID2 crowd.sbatch)
+    echo "$i Queued $LASTID2"
+done
