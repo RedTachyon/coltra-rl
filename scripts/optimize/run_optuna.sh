b595d3c3022d8ccd330bb33719cfc713bb1fa51f
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-03-25 00:26:53 +0100

    Change how optuna dbase is passed

--- scripts/optimize/run_optuna.sh
+++ scripts/optimize/run_optuna.sh
@@ -1,14 +1,15 @@
 COLTRA=$WORK/coltra-rl
 PROJECT=$COLTRA/scripts
 
+OPTUNA=${1:-optuna}
 
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 0 -n 1 -o polacc &
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 0 -n 1 -o $OPTUNA &
 sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 10 -n 1 -o polacc &
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 10 -n 1 -o $OPTUNA &
 sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 20 -n 1 -o polacc &
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 20 -n 1 -o $OPTUNA &
 sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 30 -n 1 -o polacc &
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 30 -n 1 -o $OPTUNA &
 sleep 1
-python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 40 -n 1 -o polacc &
+python $PROJECT/optimize/optuna_crowd.py -e $COLTRA/builds/crowd-v5/crowd.x86_64 -w 40 -n 1 -o $OPTUNA &
 wait
