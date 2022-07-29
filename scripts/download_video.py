d3c770c4aae6960bc08a9dea0b10de711b3ded8f
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-07-29 12:51:18 +0200

    Improve vis script

--- scripts/download_video.py
+++ scripts/download_video.py
@@ -40,15 +40,17 @@ if __name__ == "__main__":
             stderr=subprocess.PIPE,
         )
         print(out1.stdout.decode("utf-8"))
+        print(out1.stderr.decode("utf-8"))
 
     print("Generating video on remote")
     out2 = subprocess.run(
-        f"ssh -t ariel.geovic '/home/ariel/anaconda3/envs/coltra/bin/python /home/ariel/projects/coltra-rl/scripts/enjoy_crowd.py -p /home/ariel/temp/{model_name} -e /home/ariel/projects/coltra-rl/builds/crowd-v6a/crowd.x86_64'" + "-d" if args.deterministic else "",
+        f"ssh -t ariel.geovic '/home/ariel/anaconda3/envs/coltra/bin/python /home/ariel/projects/coltra-rl/scripts/enjoy_crowd.py -p /home/ariel/temp/{model_name} -e /home/ariel/projects/coltra-rl/builds/crowd-v6a/crowd.x86_64{' -d' if args.deterministic else ''}'",
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
     print(out2.stdout.decode("utf-8"))
+    print(out2.stderr.decode("utf-8"))
 
     print("Downloading video on remote")
     out3 = subprocess.run(
@@ -58,15 +60,17 @@ if __name__ == "__main__":
         stderr=subprocess.PIPE,
     )
     print(out3.stdout.decode("utf-8"))
+    print(out3.stderr.decode("utf-8"))
 
     print("Converting video to mp4")
     out4 = subprocess.run(
-        f"ffmpeg -f webm -i {args.target_dir}/video.webm {args.target_dir}/{args.name}.mp4",
+        f"ffmpeg -y -f webm -i {args.target_dir}/video.webm {args.target_dir}/{args.name}.mp4",
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
     print(out4.stdout.decode("utf-8"))
+    print(out4.stderr.decode("utf-8"))
 
     print("Cleaning up")
     out5 = subprocess.run(
@@ -76,5 +80,6 @@ if __name__ == "__main__":
         stderr=subprocess.PIPE,
     )
     print(out5.stdout.decode("utf-8"))
+    print(out5.stderr.decode("utf-8"))
 
     print("Done")
