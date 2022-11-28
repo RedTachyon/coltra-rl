de302a45dc8da87b866a7431e1980db0b339f91a
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-11-28 15:49:56 +0100

    Comment out redundant stubs

--- coltra/research/rewards/__init__.py
+++ coltra/research/rewards/__init__.py
@@ -7,28 +7,28 @@ import numpy as np
 from coltra.data_utils import Trajectory
 
 
-def evaluate_trajectory(trajectory: Trajectory, reward_function: Callable[[np.ndarray, np.ndarray], float], gamma: float = 0.99, eta: float = 0.0) -> float:
-    """Evaluate a trajectory according to the reward function.
-
-    Args:
-        trajectory (Trajectory): Trajectory to evaluate.
-        reward_function:
-        discount (float): Discount factor.
-
-    Returns:
-        float: Trajectory evaluation.
-    """
-    pass
-
-
-def annotate_actions(trajectory: Trajectory, dynamics: str = "CarVel") -> np.ndarray:
-    """Annotate the actions of a trajectory with the next state, next action, and reward.
-
-    Args:
-        trajectory (Trajectory): Trajectory to annotate.
-        dynamics (str, optional): Dynamics model to use. Defaults to "CarVel".
-
-    Returns:
-        Trajectory: Annotated trajectory.
-    """
-    pass
+# def evaluate_trajectory(trajectory: Trajectory, reward_function: Callable[[np.ndarray, np.ndarray], float], gamma: float = 0.99, eta: float = 0.0) -> float:
+#     """Evaluate a trajectory according to the reward function.
+#
+#     Args:
+#         trajectory (Trajectory): Trajectory to evaluate.
+#         reward_function:
+#         discount (float): Discount factor.
+#
+#     Returns:
+#         float: Trajectory evaluation.
+#     """
+#     pass
+#
+#
+# def annotate_actions(trajectory: Trajectory, dynamics: str = "CarVel") -> np.ndarray:
+#     """Annotate the actions of a trajectory with the next state, next action, and reward.
+#
+#     Args:
+#         trajectory (Trajectory): Trajectory to annotate.
+#         dynamics (str, optional): Dynamics model to use. Defaults to "CarVel".
+#
+#     Returns:
+#         Trajectory: Annotated trajectory.
+#     """
+#     pass
