79db99625cd548033d588716fec88f2d40e7a9c1
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-05-25 16:47:15 +0100

    Update script runner

--- scripts/run_train_crowds.sh
+++ scripts/run_train_crowds.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Check if a worker_id argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <worker_id>"
+# Check if all arguments are provided
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <config_dir> <python_p_arg> <worker_id>"
     exit 1
 fi
 
-worker_id=$1
-config_dir="./vR0-configs/rewards"
+config_dir=$1
+python_p_arg=$2
+worker_id=$3
 
 switch_flag=0
 
@@ -21,5 +22,5 @@ for file in "$config_dir"/*; do
 		switch_flag=0
 	fi
 
-	python train_crowd.py -c "$file" -i 1000 -e ../builds/crowd-vR1a/crowd.x86_64 -p crowd-reward-choke -w "$current_worker_id" -u
+	python train_crowd.py -c "$file" -i 1000 -e ../builds/crowd-vR2/crowd.x86_64 -p "$python_p_arg" -w "$current_worker_id" -u
 done
