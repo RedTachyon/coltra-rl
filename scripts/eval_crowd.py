fbf156c92b57aed8fd73ca32bca8f0091d726152
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-06-15 14:12:43 +0200

    Updated eval script, need to test

--- scripts/eval_crowd.py
+++ scripts/eval_crowd.py
@@ -1,17 +1,15 @@
 import os
 from typing import Optional
 
-import numpy as np
-import yaml
-import torch
 import wandb
+import yaml
+import numpy as np
+import torch
 from mlagents_envs.exception import UnityEnvironmentException
 from typarse import BaseParser
-from coltra.agents import CAgent
 from coltra.groups import HomogeneousGroup
-from coltra.trainers import PPOCrowdTrainer
-from coltra.training_utils import evaluate
 from coltra.envs.unity_envs import UnitySimpleCrowdEnv
+from tqdm import trange
 
 
 class Parser(BaseParser):
@@ -20,16 +18,57 @@ class Parser(BaseParser):
     project: Optional[str] = None
 
     _help = {
-        "agent_path": "Path to the trained agent",
         "env": "Path to the Unity environment binary",
         "project": "Name of wandb project",
+        "agent_path": "Path to the trained agent",
     }
 
     _abbrev = {
-        "agent_path": "a",
         "env": "e",
         "project": "p",
+        "agent_path": "a",
     }
+
+
+def evaluate(
+        env: UnitySimpleCrowdEnv,
+        group: HomogeneousGroup,
+        n_episodes: int = 5,
+        n_steps: int = 200,
+        disable_tqdm: bool = False,
+        reset_kwargs: Optional[dict] = None,
+) -> dict:
+    if reset_kwargs is None:
+        reset_kwargs = {}
+
+    metrics = {
+        "returns": [],
+        "e_energy": [],
+        "e_energyComplex": [],
+        "e_energyPlus": [],
+        "e_energyComplexPlus": []
+    }
+    current_return = 0.0
+    for ep in trange(n_episodes, disable=disable_tqdm):
+        obs = env.reset(**reset_kwargs)
+        for t in range(n_steps):
+            action, _, _ = group.act(obs)
+            obs, reward, done, info = env.step(action)
+
+            mean_reward = np.mean(list(reward.values()))
+            current_return += mean_reward
+            if all(done.values()):
+                metrics["returns"].append(current_return)
+                for key in metrics.keys():
+                    if key != "returns":
+                        metrics[key].append(info[key])
+                current_return = 0.0
+                break
+
+    for key, values in metrics.items():
+        metrics[key] = np.array(values)
+
+    return metrics
 
 
 if __name__ == "__main__":
@@ -38,7 +77,6 @@ if __name__ == "__main__":
 
         args = Parser()
 
-        # We're assuming the config for the evaluation is at the same location as the trained agent
         with open(os.path.join(args.agent_path, "full_config.yaml"), "r") as f:
             config = yaml.load(f.read(), yaml.Loader)
 
@@ -47,21 +85,11 @@ if __name__ == "__main__":
         env_config = config["environment"]
         model_type = config["model_type"]
 
-        # Initialize the environment
-        env = UnitySimpleCrowdEnv(
-            file_name=args.env,
-            no_graphics=True,
-            extra_params=env_config
-        )
-
-        env.reset()
-
-        # Initialize the agent
         wandb.init(
             project="crowdai" if args.project is None else args.project,
             sync_tensorboard=True,
             config=config,
-            name=trainer_config["tensorboard_name"],
+            name="eval_" + trainer_config["tensorboard_name"],
         )
 
         agents = HomogeneousGroup.load(args.agent_path, weight_idx=-1)
@@ -71,31 +99,46 @@ if __name__ == "__main__":
 
         print("Evaluating...")
 
-        for num_agents in range(1, 51):
-            env_config["num_agents"] = num_agents
+        scenarios = {
+            "Circle": [1, 2, 5, 10, 20, 40, 50, 70],
+            "Crossway": [2, 8, 18, 32, 50, 72],
+            "Corridor": [2, 8, 18, 32, 50, 72],
+            "Choke": [1, 2, 5, 10, 20],
+            "Car": [1, 2, 5, 10],
+        }
 
-            env.reset(**env_config)
+        for scenario, nums_agents in scenarios.items():
+            for num_agents in nums_agents:
+                env_config["num_agents"] = num_agents
+                env_config["initializer"] = scenario
 
-            performances, energies = evaluate(env, agents, 10, disable_tqdm=False)
+                env = UnitySimpleCrowdEnv(
+                    file_name=args.env,
+                    no_graphics=True,
+                    extra_params=env_config
+                )
 
-            wandb.log(
-                {
-                    f"eval/num_agents_{num_agents}/mean_episode_reward": np.mean(performances),
-                    f"eval/num_agents_{num_agents}/std_episode_reward": np.std(performances),
-                    f"eval/num_agents_{num_agents}/mean_episode_energy": np.mean(energies),
-                    f"eval/num_agents_{num_agents}/std_episode_energy": np.std(energies),
-                },
-                commit=False,
-            )
+                env.reset()
 
-        wandb.log({})
-        env.close()
+                metrics = evaluate(env, agents, n_episodes=5, n_steps=200, disable_tqdm=False)
+
+                for metric, values in metrics.items():
+                    wandb.log(
+                        {
+                            f"eval/{scenario}/{metric}": np.mean(values),
+                            f"eval/{scenario}/{metric}_std": np.std(values),
+                        },
+                        step=num_agents,
+                    )
+
+                env.close()
+
         wandb.finish()
 
     finally:
         print("Cleaning up")
         try:
-            env.close()  # pytype: disable=name-error
+            env.close()
             print("Env closed")
         except NameError:
             print("Env wasn't created. Exiting coltra")
