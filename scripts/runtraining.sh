07addd9495604bb7baa445dd396846d5abd02df3
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-09-02 22:04:17 +0200

    Postphd cleanup (#25)
    * Remove a bunch of old code, start some profiling
    
    * Bump gymnasium version
    
    * Fix a bug in MLP models
    
    * Try to make it a nice package

--- scripts/runtraining.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python train_crowd.py -c configs/crowd_configs/base.yaml -i 2000 -e ../builds/crowd-v5/crowd.x86_64 -n car_vel_new_highent -d CartesianVelocity -o Absolute -p crowdai-v5
